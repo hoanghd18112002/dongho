61702ac593d508724a95b02b6750028bfd4c63fa
Nguyencuongtt2002 <nguyencuongtt2002@gmail.com>
2024-05-13 21:58:46 +0700

    22:00 13/05/2024

--- front-end/src/Components/Client/ThongTinUser..js
+++ front-end/src/Components/Client/ThongTinUser..js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Container, Row, Col, Card } from 'react-bootstrap';
-
 const ThongTinUser = () => {
     const user = useSelector((state) => state.user.account);
     return (
